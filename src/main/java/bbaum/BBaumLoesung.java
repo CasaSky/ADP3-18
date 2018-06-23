b5172569d9cb0ed4cea39f6877080ea292172af1
Talal <talal.tabia@haw-hamburg.de>
2018-06-23 23:04:50 +0200

    first commit

---
+++ src/main/java/bbaum/BBaumLoesung.java
@@ -0,0 +1,13 @@
+package bbaum;
+
+public class BBaumLoesung<T extends Comparable<T>, U> extends BBaum {
+
+    public BBaumLoesung(int ordnung) {
+        super(ordnung);
+    }
+
+    @Override
+    public void einfuegen(Comparable schluessel, Object wert) {
+
+    }
+}
